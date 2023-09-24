1c41cd5b70cf054bad4d994848fb86d43c964826
Satyam0327 <satyamsingh0327@gmail.com>
2023-09-24 14:14:40 +0530

    done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,54 +1,91 @@
-import css from './Statistics.module.css'
-import { BsArrowUpShort } from 'react-icons/bs'
-import { groupNumber } from '../../data'
-import StatisticsChart from '../StatisticsChart/StatisticsChart'
-
+import React, { useRef, useEffect, useState } from 'react';
+import { BsArrowUpShort } from 'react-icons/bs';
+import { groupNumber } from '../../data';
+import StatisticsChart from '../StatisticsChart/StatisticsChart';
+import css from './Statistics.module.css';
 
 const Statistics = () => {
-    return (    <>
-        <div className={`${css.container} theme-container`}>
-            <span className={css.title}>Overview Statistics</span>
-            <div className={`${css.cards} grey-container`}>
-                <div>
-                    <div className={css.arrowIcon}>
-                        <BsArrowUpShort />
-                    </div>  
+  // Create a ref for the theme-container2 element
+  const themeContainer2Ref = useRef(null);
+  const [isThemeContainer2Visible, setIsThemeContainer2Visible] = useState(false);
 
-                    <div className={css.card}>
-                        <span>Water Depth </span><span>Water Depth Analysis</span>
-                    </div>
-                </div>
+  // Function to handle intersection observer callback
+  const handleIntersection = (entries) => {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      setIsThemeContainer2Visible(true);
+    }
+  };
 
-                <div className={css.card}>
-                    <span>Water supply</span><span>{groupNumber(455)}</span>
-                </div>
+  // Initialize Intersection Observer
+  useEffect(() => {
+    const options = {
+      root: null, // viewport
+      rootMargin: '0px',
+      threshold: 0.5, // 50% of the element must be visible
+    };
 
-                <div className={css.card}>
-                    <span>Present supply</span><span>{groupNumber(370000)}</span>
-                </div>
+    const observer = new IntersectionObserver(handleIntersection, options);
 
-                <div className={css.card}>
-                    <span> Average Depth analysis</span><span> {groupNumber(2000)}</span>
-                </div>
+    // Observe the theme-container2 element
+    if (themeContainer2Ref.current) {
+      observer.observe(themeContainer2Ref.current);
+    }
+
+    // Clean up the observer when the component unmounts
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className={`${css.container} theme-container`}>
+        <span className={css.title}>Overview Statistics</span>
+        <div className={`${css.cards} grey-container`}>
+          <div>
+            <div className={css.arrowIcon}>
+              <BsArrowUpShort />
             </div>
-            <StatisticsChart/>
-        </div>
-        <div className={`${css.container} theme-container`}>
-            <span className={css.title}>Current results </span>
-            <div className={`${css.cards} grey-container`}>
-                <div className={css.card}>
-                    <span >Digging can be done here : </span><span>Yes</span>
-                </div>
-                <div className={css.card}>
-                    <span>Digging Method should be used here:</span><span>Rotary Drilling</span>
-                </div>
-                <div className={css.card}>
-                    <span></span><span></span>
-                </div>
-            </div>
-        </div>
-        </>
-    )
-}
 
-export default Statistics
+            <div className={css.card}>
+              <span>Water Depth</span><span>Water Depth Analysis</span>
+            </div>
+          </div>
+
+          <div className={css.card}>
+            <span>Water supply</span><span>{groupNumber(455)}</span>
+          </div>
+
+          <div className={css.card}>
+            <span>Present supply</span><span>{groupNumber(370000)}</span>
+          </div>
+
+          <div className={css.card}>
+            <span> Average Depth analysis</span><span> {groupNumber(2000)}</span>
+          </div>
+        </div>
+        <StatisticsChart />
+      </div>
+      <div
+        className={`${css.container} ${isThemeContainer2Visible ? 'animate' : ''} theme-container2`}
+        ref={themeContainer2Ref}
+      >
+        <span className={css.title}>Current results </span>
+        <div className={`${css.cards} grey-container`}>
+          <div className={css.card}>
+            <span>Digging can be done here:</span><span>Yes</span>
+          </div>
+          <div className={css.card}>
+            <span>Digging Method should be used here:</span><span>Rotary Drilling</span>
+          </div>
+          <div className={css.card}>
+            <span></span><span></span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Statistics;
